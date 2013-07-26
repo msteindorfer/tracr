d39272cc4fcd9ab491b447f315bd96b56c5ba7d2
Michael Steindorfer <Michael.Steindorfer@cwi.nl>
2013-07-26 14:53:53 +0200

    Adding local maven repo and dependency for pdb.values (for future use of binary serialisation).

--- project/Build.scala
+++ project/Build.scala
@@ -5,6 +5,10 @@ object ApplicationBuild extends Build {
 
   val appName = "tracr"
 
+  val localRepo = Seq(
+    "Local Maven Repository" at "file://" + Path.userHome.absolutePath + "/.m2/repository"
+  )
+
   val sonatypeRepo = Seq(
     "Sonatype repositosnapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
     "Sonatype repository releases" at "http://oss.sonatype.org/content/repositories/releases/"
@@ -21,9 +25,10 @@ object ApplicationBuild extends Build {
       fork := true,
       scalaVersion := "2.10.2",
       javaOptions in run += "-Xmx8G",
-      resolvers ++= mandubianRepo ++ sonatypeRepo,
+      resolvers ++= localRepo ++ sonatypeRepo ++ mandubianRepo,
       libraryDependencies ++= Seq(
         "play" %% "play-json" % "2.2-SNAPSHOT",
+        "org.eclipse.imp" % "org.eclipse.imp.pdb.values" % "0.4.1.qualifier",
         "org.specs2" %% "specs2" % "1.13" % "test",
         "junit" % "junit" % "4.11" % "test"
       )
