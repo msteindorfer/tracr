d05dcf1e4dc9bc6b7b9412b29d334ee4191d0bc2
Michael Steindorfer <Michael.Steindorfer@cwi.nl>
2013-11-09 21:42:13 +0100

    Not saving bash output anymore.

--- runAB.sh
+++ runAB.sh
@@ -1,2 +1,2 @@
-sbt run > _overlaps_with_different_sizes_a.log
-sbt "run sharingEnabled" > _overlaps_with_different_sizes_b.log
+sbt run # > _overlaps_with_different_sizes_a.log
+sbt "run sharingEnabled" # > _overlaps_with_different_sizes_b.log
