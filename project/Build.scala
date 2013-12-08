6aed60a5bd1a989ce096c7b6f09e8b91fb1427c8
Michael Steindorfer <Michael.Steindorfer@cwi.nl>
2013-12-08 17:20:07 +0100

    Comment out unused dependencies.

--- project/Build.scala
+++ project/Build.scala
@@ -28,8 +28,8 @@ object ApplicationBuild extends Build {
       javaOptions in run += "-Xmx24G",
       resolvers ++= localRepo ++ sonatypeRepo ++ mandubianRepo,
       libraryDependencies ++= Seq(
-        "play" %% "play-json" % "2.2-SNAPSHOT",
-        "org.eclipse.imp" % "org.eclipse.imp.pdb.values" % "0.4.1.qualifier",
+//        "play" %% "play-json" % "2.2-SNAPSHOT",
+//        "org.eclipse.imp" % "org.eclipse.imp.pdb.values" % "0.4.1.qualifier",
         "com.google.protobuf" % "protobuf-java" % "2.5.0",
         "org.specs2" %% "specs2" % "1.13" % "test",
         "junit" % "junit" % "4.11" % "test"
